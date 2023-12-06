c9082a5c509f147640cc1f8dc70509b358861fdd
aircongo <natelufuluabo@yahoo.ca>
2023-12-06 13:58:48 -0500

    implemented the help flag for the cli tool

--- bin/index.js
+++ bin/index.js
@@ -3,38 +3,34 @@
 import path from 'path';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
-// import { executeCommand } from '../wordCount.js';
+import { executeCommand } from '../wordCount.js';
 
 const usage = "\nUsage: ccwc <flag> file to run the command on"
 
-const options = yargs()
+const options = yargs(hideBin(process.argv))
     .usage(usage)
-    .option(
-        "w", 
-        {
-            alias: "count_words", 
-            describe: "counts the number of words in a the file",
-            type: "boolean",
-            demandOption: false
+    .options({
+        'count_words': {
+            alias: 'w',
+            describe: 'counts the number of words in a the file',
+            demandOption: false,
+            type: 'boolean'
         },
-    )
-    .option(
-        "c", 
-        {
-            alias: "count_byte", 
-            describe: "counts the number of bytes in a the file",
-            type: "boolean",
-            demandOption: false
+        'count_bytes': {
+            alias: 'c',
+            describe: 'counts the number of bytes in a the file',
+            demandOption: false,
+            type: 'boolean'
         },
-    )
-    .option(
-        "l", 
-        {
-            alias: "count_lines", 
-            describe: "counts the number of lines in a the file",
-            type: "boolean",
-            demandOption: false
+        'count_lines': {
+            alias: 'l',
+            describe: 'counts the number of lines in a the file',
+            demandOption: false,
+            type: 'boolean'
         },
-    )
+    })
+    .version(true)
     .help(true)
     .argv;
+
+console.log(process.argv[2]);

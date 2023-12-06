0e5e3cb05c8e805fa243da7499836a249bbb017e
aircongo <natelufuluabo@yahoo.ca>
2023-12-06 13:04:42 -0500

    rewrote the index file to add usage information

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,40 @@
+#! /usr/bin/env node 
+
 import path from 'path';
-import { executeCommand } from '../wordCount.js';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers'
+// import { executeCommand } from '../wordCount.js';
 
-const cliArguments = process.argv[2];
+const usage = "\nUsage: ccwc <flag> file to run the command on"
 
-// Specify the relative path to the file
-const fileToUse = process.argv[3];
-
-// Resolve the absolute path
-const absolutePath = path.resolve(fileToUse);
-
-if (cliArguments === '-c') console.log(await executeCommand(absolutePath))
+const options = yargs()
+    .usage(usage)
+    .option(
+        "w", 
+        {
+            alias: "count_words", 
+            describe: "counts the number of words in a the file",
+            type: "boolean",
+            demandOption: false
+        },
+    )
+    .option(
+        "c", 
+        {
+            alias: "count_byte", 
+            describe: "counts the number of bytes in a the file",
+            type: "boolean",
+            demandOption: false
+        },
+    )
+    .option(
+        "l", 
+        {
+            alias: "count_lines", 
+            describe: "counts the number of lines in a the file",
+            type: "boolean",
+            demandOption: false
+        },
+    )
+    .help(true)
+    .argv;

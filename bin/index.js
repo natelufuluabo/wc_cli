2d6ce51ad8ce04650588f08746eb2d85f13c4e31
aircongo <natelufuluabo@yahoo.ca>
2023-12-07 07:25:10 -0500

    added the m flag among option for the cli app

--- bin/index.js
+++ bin/index.js
@@ -9,12 +9,6 @@ const usage = "\nUsage: ccwc <flag> file to run the command on"
 const options = yargs(hideBin(process.argv))
     .usage(usage)
     .options({
-        'count_words': {
-            alias: 'w',
-            describe: 'counts the number of words in a the file',
-            demandOption: false,
-            type: 'boolean'
-        },
         'count_bytes': {
             alias: 'c',
             describe: 'counts the number of bytes in a the file',
@@ -24,6 +18,18 @@ const options = yargs(hideBin(process.argv))
         'count_lines': {
             alias: 'l',
             describe: 'counts the number of lines in a the file',
+            demandOption: false,
+            type: 'boolean'
+        },
+        'count_words': {
+            alias: 'w',
+            describe: 'counts the number of words in a the file',
+            demandOption: false,
+            type: 'boolean'
+        },
+        'count_characters': {
+            alias: 'm',
+            describe: 'counts the number of characters in a the file',
             demandOption: false,
             type: 'boolean'
         },

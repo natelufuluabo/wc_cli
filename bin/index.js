1fe6a35edc322bbf33519cf594ff58321ff0a6dc
aircongo <natelufuluabo@yahoo.ca>
2023-12-07 07:46:24 -0500

    updated usage documentation

--- bin/index.js
+++ bin/index.js
@@ -4,32 +4,32 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { executeCommand } from '../utils_functions/main.js';
 
-const usage = "\nUsage: ccwc <flag> file to run the command on"
+const usage = "\nUsage: ccwc <flag> <file>"
 
 const options = yargs(hideBin(process.argv))
     .usage(usage)
     .options({
         'count_bytes': {
             alias: 'c',
-            describe: 'counts the number of bytes in a the file',
+            describe: 'counts the number of bytes in the file',
             demandOption: false,
             type: 'boolean'
         },
         'count_lines': {
             alias: 'l',
-            describe: 'counts the number of lines in a the file',
+            describe: 'counts the number of lines in the file',
             demandOption: false,
             type: 'boolean'
         },
         'count_words': {
             alias: 'w',
-            describe: 'counts the number of words in a the file',
+            describe: 'counts the number of words in the file',
             demandOption: false,
             type: 'boolean'
         },
         'count_characters': {
             alias: 'm',
-            describe: 'counts the number of characters in a the file',
+            describe: 'counts the number of characters in the file',
             demandOption: false,
             type: 'boolean'
         },

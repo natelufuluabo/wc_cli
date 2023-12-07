bd09af20874f0f19bffedfacec2f088096fd43ff
aircongo <natelufuluabo@yahoo.ca>
2023-12-07 08:11:11 -0500

    implemented a function that returns the content of a file

---
+++ utils_functions/getFileContent.js
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+
+export async function getFileContentOrData(input) {
+  try {
+    // Check if the input is a file path
+    const isFilePath = await fs.stat(input).then((stats) => stats.isFile());
+
+    if (isFilePath) {
+      // If it's a file path, read the file and return its content
+      const fileContent = await fs.readFile(input, 'utf8');
+      return fileContent;
+    } else {
+      // If it's not a file path, assume it's content and return it
+      return input;
+    }
+  } catch (error) {
+    // Handle errors, e.g., file not found
+    console.error('Error:', error.message);
+    throw error;
+  }
+}
